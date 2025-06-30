535a6ba3c297394648b83d778a920d7c2d73eff5
Shreyash Bhagwat <shreyashbhagwat505@gmail.com>
2025-06-30 21:45:24 +0530

    commiting few chaanges

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,3 +1,14 @@
-const Discover = () => <div>Discover</div>;
+import { Error,Loader,SongCard } from "../components";
+import {genres} from '../assets/constants';
 
+
+const Discover = () => {
+    console.log(genres);
+
+
+ return(
+    <div className="flex flex-col">Discover</div>
+ )
+
+ }
 export default Discover;
